525fc9cc82eabc1ea8920e22fd4c06a17c56850f
Rodrigo Rocha Sierra <rrs230780@gmail.com>
2025-04-27 18:08:02 -0500

    nuevo

--- 04_tuples.py
+++ 04_tuples.py
@@ -3,3 +3,4 @@ my_other_tuple = ()
 
 my_tuple = (44, 1.76, 'Rodrigo', 'Rocha')
 print(my_tuple)
+
--- funciones_basicas.py
+++ funciones_basicas.py
@@ -1,10 +1,11 @@
-# definicion de la funcion
-def suma(a, b):
+# definicion de la funcion def suma(a, b):
     return  a + b
 
 primer_numero = int(input('Ingrese el primer Numero: '))
 segundo_numero = int(input('Ingresa el segundo Numero:'))
 resultado = suma(primer_numero, segundo_numero)
 print(resultado)
+
+
 
 
